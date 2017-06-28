5dd13c472db2ba21a1ca9892a0c89fcec5bc1906
Ariki Molloy <tuariki.d.molloy@leidos.com>
2017-06-28 08:41:58 -0400

    adds ability to work with java formats escaping text by using single quotes

--- moment-jdateformatparser.js
+++ moment-jdateformatparser.js
@@ -125,7 +125,20 @@
      */
     moment.fn.toMomentFormatString = function (formatString) {
       if (!javaDateFormats[formatString]) {
-        javaDateFormats[formatString] = translateFormat(formatString, javaFormatMapping);
+          var mapped = "";
+          var regexp = /[^']+|('[^']*')/g;
+
+          while ((part = regexp.exec(formatString))) {
+              part = part[0];
+              if (part.match(/'.?'/)) {
+                  mapped += "[" + part.substring(1, part.length - 1) + "]";
+              }
+              else {
+                  mapped += translateFormat(part, javaFormatMapping);
+              }
+          }
+
+          javaDateFormats[formatString] = mapped;
       }
       return javaDateFormats[formatString];
     };
